c3a221dfde31ae8cdb95e8258fa66fe4d92f7f93
Guilherme Machado <guilherme.machado@indicium.tech>
2023-11-28 02:10:22 -0300

    feat: updating pydantic

--- app/routers/schemas.py
+++ app/routers/schemas.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, constr
 
 class CustomersSchema(BaseModel):
-    customer_id : str
-    customer_unique_id : str
-    customer_zip_code_prefix : Optional[str] = None
-    customer_city : Optional[str] = None
-    customer_state : Optional[str] = None
+    customer_id: constr(min_length=32, max_length=32)
+    customer_unique_id: constr(min_length=32, max_length=32)
+    customer_zip_code_prefix: Optional[str] = None
+    customer_city: Optional[str] = None
+    customer_state: Optional[str] = None
