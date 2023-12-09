6a384b2c34de91935f356b7156af91165ab4755a
Guilherme Machado <guilherme.machado@indicium.tech>
2023-12-09 00:44:34 -0300

    tests: updated tests

--- tests/test_closed_deals_endpoint.py
+++ tests/test_closed_deals_endpoint.py
@@ -1,26 +1,27 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from app.main import app
+from api.app.main import app
 
 client = TestClient(app)
 
+
 def test_add_closed_deals():
     payload = {
-    "mql_id": "5420aad7fec3549a85876ba1c529bd84",
-    "seller_id": "2c43fb513632d29b3b58df74816f1b06",
-    "sdr_id": "a8387c01a09e99ce014107505b92388c",
-    "sr_id": "4ef15afb4b2723d8f3d81e51ec7afefe",
-    "won_date": "2018-02-26 19:58:54",
-    "business_segment": "pet",
-    "lead_type":"online_medium",
-    "lead_behaviour_profile": "cat",
-    "has_company":None,
-    "has_gtin": None,
-    "average_stock": None,
-    "business_type": "reseller",
-    "declared_product_catalog_size": None,
-    "declared_monthly_revenue": 0.0
+        "mql_id": "5420aad7fec3549a85876ba1c529bd84",
+        "seller_id": "2c43fb513632d29b3b58df74816f1b06",
+        "sdr_id": "a8387c01a09e99ce014107505b92388c",
+        "sr_id": "4ef15afb4b2723d8f3d81e51ec7afefe",
+        "won_date": "2018-02-26 19:58:54",
+        "business_segment": "pet",
+        "lead_type": "online_medium",
+        "lead_behaviour_profile": "cat",
+        "has_company": None,
+        "has_gtin": None,
+        "average_stock": None,
+        "business_type": "reseller",
+        "declared_product_catalog_size": None,
+        "declared_monthly_revenue": 0.0,
     }
 
     response = client.post("/closed_deals", json=payload)
--- tests/test_marketing_qualified_leads.py
+++ tests/test_marketing_qualified_leads.py
@@ -1,14 +1,15 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from app.main import app
+from api.app.main import app
 
 client = TestClient(app)
 
+
 def test_add_marketing_qualified_leads():
     payload = {
-    "mql_id": "5420aad7fec3549a85876ba1c529bd84",
-    "landing_page_id": "a8387c01a09e99ce014107505b92388c"
+        "mql_id": "5420aad7fec3549a85876ba1c529bd84",
+        "landing_page_id": "a8387c01a09e99ce014107505b92388c",
     }
 
     response = client.post("/marketing_qualified_leads", json=payload)
